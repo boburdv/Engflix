3e09b84af91b9205d993d48529d7a1696636af29
boburdv <boburdv@gmail.com>
2026-01-26 12:34:45 -0500

    Update Card component

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 export default function Card({ movie }) {
   return (
     <Link href={`/watch/${movie.id}`}>
-      <div className="cursor-pointer">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full max-h-72 object-cover" />
+      <div className="cursor-pointer group">
+        <div className="overflow-hidden">
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full max-h-72 object-cover transition-transform duration-400 group-hover:scale-105" />
+        </div>
 
         <div className="mt-7 flex flex-col gap-4">
           <div className="flex justify-between">
@@ -14,16 +16,16 @@ export default function Card({ movie }) {
           </div>
 
           <div className="flex justify-between">
-            <img src="/hd.svg" />
+            <img src="/hd.svg" alt="HD" />
 
             <div className="text-[12px] flex gap-5">
               <p className="flex items-center gap-1">
-                <img src="/time-icon.svg" width={15} />
+                <img src="/time-icon.svg" width={15} alt="Time" />
                 {movie.runtime ? `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}m` : "-"}
               </p>
 
               <p className="flex gap-1">
-                <img src="/star.svg" width={13} />
+                <img src="/star.svg" width={13} alt="Rating" />
                 {movie.vote_average.toFixed(1)}
               </p>
             </div>
